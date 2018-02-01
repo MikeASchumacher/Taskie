09e8d516615b0dd46e5504c14e67975d900e756c
MikeASchumacher <MikeASchumacher@gmail.com>
2018-01-31 22:44:34 -0500

    Add functionality Complete. Quotes replace hint in EditView Dialog. Cosmetic changes.

--- app/src/main/java/com/mikeaschumacher/tasky/MainActivity.java
+++ app/src/main/java/com/mikeaschumacher/tasky/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mikeaschumacher.tasky;
 
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.ContentValues;
@@ -17,9 +18,12 @@ import android.support.annotation.RequiresApi;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -46,6 +50,7 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -71,12 +76,39 @@ public class MainActivity extends AppCompatActivity {
     private TaskDbHelper mHelper;
     private String tempTaskName;
 
+    //used for changing the hint of the EditText in the "add" dialog
+    String hints[] = new String[]{
+            "The best way to predict your future is to create it." + "\n" + "\n" + "Unknown",
+            "If your ship doesn't come in, swim out to it." + "\n" + "\n" + "Johnathan Winters",
+            "Do, or do not. There is no 'try'." + "\n" + "\n" + "Yoda",
+            "Motivation is what gets you started. Habit is what keeps you going." + "\n" + "\n" + "Jim Ryun",
+            "You will never find time for anything. If you want time you must make it." + "\n" + "\n" + "Charles Buxton",
+            "If we did all we were capable of doing, we would literally astonish ourselves." + "\n" + "\n" + "Thomas Edison",
+            "Will you look back on life and say, \"I wish I had,\" or \"I'm glad I did\"?" + "\n" + "\n" + "Zig Ziglar",
+            "You miss 100% of the shots you don’t take." + "\n" + "Wayne Gretzky -Michael Scott",
+            "The most difficult thing is the decision to act, the rest is merely tenacity." + "\n" + "\n" + " Amelia Earhart",
+            "Definiteness of purpose is the starting point of all achievement." + "\n" + "\n" + "W. Clement Stone",
+            "We become what we think about." + "\n" + "Earl Nightingale",
+            "The best time to plant a tree was 20 years ago. The second best time is now." + "\n" + "\n" + "Chinese Proverb",
+            "Your time is limited, so don’t waste it living someone else’s life." + "\n" + "\n" + "Steve Jobs",
+            "Either you run the day, or the day runs you." + "\n" + "\n" + "Jim Rohn",
+            "The best revenge is massive success." + "\n" + "\n" + "Frank Sinatra",
+            "Believe you can and you’re halfway there." + "\n" + "\n" + "Theodore Roosevelt",
+            "Happiness is not something readymade. It comes from your own actions." + "\n" + "\n" + "Dalai Lama",
+            "If the wind will not serve, take to the oars." + "\n" + "\n" + "Latin Proverb",
+            "Too many of us are not living our dreams because we are living our fears." + "\n" + "\n" + "Les Brown",
+            "A person who never made a mistake never tried anything new." + "\n" + "\n" + "Albert Einstein",
+            "I would rather die of passion than of boredom." + "\n" + "\n" + "Vincent van Gogh",
+            "It does not matter how slowly you go as long as you do not stop." + "\n" + "\n" + "Confucius"
+    };
+
+    //listener for Date Selection, stores the date selected by used and calls addItem
     private DatePickerDialog.OnDateSetListener myDatePickerListener = new DatePickerDialog.OnDateSetListener() {
         public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
             MainActivity.this.yearX = year;
             MainActivity.this.monthX = month + 1;
             MainActivity.this.dayX = dayOfMonth;
-            Toast.makeText(MainActivity.this, "Task Added: " + MainActivity.this.monthX + " / " + MainActivity.this.dayX + " / " + MainActivity.this.yearX, Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, tempTaskName + " added", Toast.LENGTH_SHORT).show();
             view.updateDate(MainActivity.this.c.get(Calendar.YEAR), MainActivity.this.c.get(Calendar.MONTH), MainActivity.this.c.get(Calendar.DAY_OF_MONTH));
             MainActivity.this.onAddItem();
         }
@@ -139,30 +171,24 @@ public class MainActivity extends AppCompatActivity {
     //Attaches a long click listener to the listview
     //Long click used for removing item from list and from database
     private void setupListViewListener() {
+
+        //listen for touch and hold to remove item from list
         this.lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             public boolean onItemLongClick(AdapterView<?> adapterView, View item, int pos, long id) {
+
+                //get database
                 SQLiteDatabase db = MainActivity.this.mHelper.getWritableDatabase();
+                //remove item from database
                 db.delete(TaskContract.TaskEntry.TABLE, "title = ?", new String[]{((TaskItem) MainActivity.this.items.get(pos)).taskName});
                 db.close();
-                Date date = new GregorianCalendar(((TaskItem) MainActivity.this.items.get(pos)).dueYear, ((TaskItem) MainActivity.this.items.get(pos)).dueMonth - 1, ((TaskItem) MainActivity.this.items.get(pos)).dueDay).getTime();
-                TaskItem tempTask = new TaskItem(((TaskItem) MainActivity.this.items.get(pos)).dueYear, ((TaskItem) MainActivity.this.items.get(pos)).dueMonth, ((TaskItem) MainActivity.this.items.get(pos)).dueDay, ((TaskItem) MainActivity.this.items.get(pos)).taskName);
+                //remove item from list adapter
                 MainActivity.this.items.remove(pos);
 
-                boolean secondTask = false;
-                Iterator it = MainActivity.this.items.iterator();
-                while (it.hasNext()) {
-                    TaskItem task = (TaskItem) it.next();
-                    if (tempTask.dueDay == task.dueDay && tempTask.dueMonth == task.dueMonth && tempTask.dueYear == task.dueYear) {
-                        secondTask = true;
-                    }
-                }
-
+                //remove item from calendar
                 events.clear();
-
                 for (TaskItem task : items) {
                     events.add(task.getDate());
                 }
-
                 cv.setEvents(events);
 
                 MainActivity.this.itemsAdapter.notifyDataSetChanged();
@@ -171,6 +197,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //create dialog for date picker
     protected Dialog onCreateDialog(int id) {
         if (id != DILOG_ID) {
             return null;
@@ -178,52 +205,71 @@ public class MainActivity extends AppCompatActivity {
         return new DatePickerDialog(this, this.myDatePickerListener, this.c.get(Calendar.YEAR), this.c.get(Calendar.MONTH), this.c.get(Calendar.DAY_OF_MONTH));
     }
 
+    //open "addition_dialog" to get info from user
+    @SuppressLint("ClickableViewAccessibility")
     @RequiresApi(api = Build.VERSION_CODES.CUPCAKE)
     public void onDateClick(View v) throws InterruptedException {
 
+        //creating dialog
         final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
         final LayoutInflater inflater = this.getLayoutInflater();
         final View dialogView = inflater.inflate(R.layout.addition_dialog, null);
         dialogBuilder.setView(dialogView);
 
+        //create button to listen for click
         Button add = dialogView.findViewById(R.id.addBtn);
 
+        //create EditText to modify hint, get user input
         final EditText mEdit = dialogView.findViewById(R.id.alertEntry);
+        mEdit.setHintTextColor(getResources().getColor(R.color.hintColor));
+        Random r = new Random();
+        mEdit.setHint(hints[r.nextInt(hints.length)]);
+        mEdit.setTextSize(16);
 
+        //create dialog
         final AlertDialog alertDialog = dialogBuilder.create();
 
+        //listen for touch on EditText to change the font size,
+        //  make cursor visible
+        mEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEdit.setTextSize(20);
+                mEdit.setHint("Task Name");
+                mEdit.setCursorVisible(true);
+            }
+        });
+
+        //handle "add" button click
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getBaseContext(), mEdit.getText(), Toast.LENGTH_SHORT).show();
                 tempTaskName = mEdit.getText().toString();
-                MainActivity.this.showDialog(MainActivity.DILOG_ID);
-                alertDialog.cancel();
+                if (!tempTaskName.equals("")) {
+                    MainActivity.this.showDialog(MainActivity.DILOG_ID);
+                    alertDialog.cancel();
+                }
+                else {
+                    Toast.makeText(getBaseContext(), "Task Needs a Name", Toast.LENGTH_LONG).show();
+                }
+
             }
         });
 
         alertDialog.show();
-
-
-        /*final EditText mEdit = new EditText(this);
-        mEdit.setSingleLine();
-        mEdit.setInputType(16385);
-        new AlertDialog.Builder(this).setTitle((CharSequence) "Add A New Task").setView(mEdit).setPositiveButton((CharSequence) "Add", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                if (mEdit.getText().toString().equals("")) {
-                    Toast.makeText(MainActivity.this, "Enter a Task Name", Toast.LENGTH_SHORT).show();
-                }
-                MainActivity.this.tempTaskName = mEdit.getText().toString();
-                MainActivity.this.showDialog(MainActivity.DILOG_ID);
-            }
-        }).setNegativeButton((CharSequence) "Cancel", null).create().show();*/
     }
 
+    //add item to the database, refresh calendar view
     public void onAddItem() {
 
         SQLiteDatabase db = this.mHelper.getWritableDatabase();
+
+        //create task to be added
         TaskItem toAdd = new TaskItem(this.yearX, this.monthX, this.dayX, this.tempTaskName);
+
+        //reset tempTaskName to avoid garbage values
         this.tempTaskName = "";
+
         ContentValues values = new ContentValues();
         values.put(TaskContract.TaskEntry.COL_TASK_TITLE, toAdd.taskName);
         values.put(TaskContract.TaskEntry.COL_DAY_TITLE, Integer.valueOf(toAdd.dueDay));
@@ -231,22 +277,25 @@ public class MainActivity extends AppCompatActivity {
         values.put("year", Integer.valueOf(toAdd.dueYear));
         db.insert(TaskContract.TaskEntry.TABLE, null, values);
         db.close();
+
         this.itemsAdapter.add(toAdd);
 
+        //refresh calendar to add event to day
         events.add(toAdd.getDate());
         cv.setEvents(events);
 
+        //reorder the items on the listview
         orderTasks();
     }
 
     //Reorders the tasks in arraylist in closest to furthest date order
     public void orderTasks() {
 
+        //bubble sort
         int i;
         ArrayList<TaskItem> temp = new ArrayList();
         int indexTracker = this.items.size();
 
-        //
         for (i = 0; i < indexTracker; i++) {
             temp.add(this.items.remove(0));
         }
@@ -268,6 +317,7 @@ public class MainActivity extends AppCompatActivity {
 
     //read tasks from the SQLite database
     private void readItems() {
+
         this.items = new ArrayList();
         Cursor curseYou = this.mHelper.getReadableDatabase().query(TaskContract.TaskEntry.TABLE, new String[]{TaskContract.TaskEntry.COL_TASK_TITLE, TaskContract.TaskEntry.COL_DAY_TITLE, "month", "year"}, null, null, null, null, null);
         while (curseYou.moveToNext()) {
@@ -307,6 +357,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
@@ -314,6 +365,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
